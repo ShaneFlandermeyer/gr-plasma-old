d1d7263816844bb3fe147425d9775be0390b18f7
shane <shaneflandermeyer@gmail.com>
2022-01-24 21:26:50 -0600

    Implemented LFM block

--- lib/linear_fm_waveform_impl.cc
+++ lib/linear_fm_waveform_impl.cc
@@ -5,51 +5,64 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-#include <gnuradio/io_signature.h>
 #include "linear_fm_waveform_impl.h"
+#include <gnuradio/io_signature.h>
 
 namespace gr {
-  namespace plasma {
+namespace plasma {
 
-    #pragma message("set the following appropriately and remove this warning")
-    using output_type = gr_complex;
-    linear_fm_waveform::sptr
-    linear_fm_waveform::make(double bandwidth, double pulsewidth, double prf, double samp_rate)
-    {
-      return gnuradio::make_block_sptr<linear_fm_waveform_impl>(
+using output_type = gr_complex;
+linear_fm_waveform::sptr linear_fm_waveform::make(double bandwidth,
+                                                  double pulsewidth,
+                                                  double prf,
+                                                  double samp_rate)
+{
+    return gnuradio::make_block_sptr<linear_fm_waveform_impl>(
         bandwidth, pulsewidth, prf, samp_rate);
+}
+
+
+/*
+ * The private constructor
+ */
+linear_fm_waveform_impl::linear_fm_waveform_impl(double bandwidth,
+                                                 double pulsewidth,
+                                                 double prf,
+                                                 double samp_rate)
+    : gr::sync_block("linear_fm_waveform",
+                     gr::io_signature::make(0, 0, 0),
+                     gr::io_signature::make(
+                         1 /* min outputs */, 1 /*max outputs */, sizeof(output_type))),
+      d_bandwidth(bandwidth),
+      d_pulsewidth(pulsewidth),
+      d_prf(prf),
+      d_samp_rate(samp_rate),
+      d_curr_index(0)
+{
+    // Create the data vector
+    LinearFMWaveform wave(bandwidth, pulsewidth, prf, samp_rate);
+    d_data = wave.pulse().cast<output_type>();
+}
+
+/*
+ * Our virtual destructor.
+ */
+linear_fm_waveform_impl::~linear_fm_waveform_impl() {}
+
+int linear_fm_waveform_impl::work(int noutput_items,
+                                  gr_vector_const_void_star& input_items,
+                                  gr_vector_void_star& output_items)
+{
+    auto out = static_cast<output_type*>(output_items[0]);
+
+    for (size_t i = 0; i < static_cast<size_t>(noutput_items); i++) {
+        out[i] = d_data[d_curr_index];
+        d_curr_index = (d_curr_index + 1) % d_data.size();
     }
 
+    // Tell runtime system how many output items we produced.
+    return noutput_items;
+}
 
-    /*
-     * The private constructor
-     */
-    linear_fm_waveform_impl::linear_fm_waveform_impl(double bandwidth, double pulsewidth, double prf, double samp_rate)
-      : gr::sync_block("linear_fm_waveform",
-              gr::io_signature::make(0, 0, 0),
-              gr::io_signature::make(1 /* min outputs */, 1 /*max outputs */, sizeof(output_type)))
-    {}
-
-    /*
-     * Our virtual destructor.
-     */
-    linear_fm_waveform_impl::~linear_fm_waveform_impl()
-    {
-    }
-
-    int
-    linear_fm_waveform_impl::work(int noutput_items,
-        gr_vector_const_void_star &input_items,
-        gr_vector_void_star &output_items)
-    {
-      auto out = static_cast<output_type*>(output_items[0]);
-
-      #pragma message("Implement the signal processing in your block and remove this warning")
-      // Do <+signal processing+>
-
-      // Tell runtime system how many output items we produced.
-      return noutput_items;
-    }
-
-  } /* namespace plasma */
+} /* namespace plasma */
 } /* namespace gr */
--- lib/linear_fm_waveform_impl.h
+++ lib/linear_fm_waveform_impl.h
@@ -9,28 +9,39 @@
 #define INCLUDED_PLASMA_LINEAR_FM_WAVEFORM_IMPL_H
 
 #include <gnuradio/plasma/linear_fm_waveform.h>
+#include <plasma-dsp/linear-fm-waveform.h>
 
+// TODO: Resolve this namespace problem
+using LinearFMWaveform = plasma::LinearFMWaveform;
 namespace gr {
-  namespace plasma {
+namespace plasma {
 
-    class linear_fm_waveform_impl : public linear_fm_waveform
-    {
-     private:
-      // Nothing to declare in this block.
+class linear_fm_waveform_impl : public linear_fm_waveform
+{
+private:
+    // Nothing to declare in this block.
+    double d_bandwidth;
+    double d_pulsewidth;
+    double d_prf;
+    double d_samp_rate;
+    size_t d_curr_index;
+    // TODO: For some reason, gnuradio dies when I make this a class member
+    // LinearFMWaveform d_waveform;
+    Eigen::ArrayXcf d_data;
+public:
+    linear_fm_waveform_impl(double bandwidth,
+                            double pulsewidth,
+                            double prf,
+                            double samp_rate);
+    ~linear_fm_waveform_impl();
 
-     public:
-      linear_fm_waveform_impl(double bandwidth, double pulsewidth, double prf, double samp_rate);
-      ~linear_fm_waveform_impl();
+    // Where all the action really happens
+    int work(int noutput_items,
+             gr_vector_const_void_star& input_items,
+             gr_vector_void_star& output_items);
+};
 
-      // Where all the action really happens
-      int work(
-              int noutput_items,
-              gr_vector_const_void_star &input_items,
-              gr_vector_void_star &output_items
-      );
-    };
-
-  } // namespace plasma
+} // namespace plasma
 } // namespace gr
 
 #endif /* INCLUDED_PLASMA_LINEAR_FM_WAVEFORM_IMPL_H */
